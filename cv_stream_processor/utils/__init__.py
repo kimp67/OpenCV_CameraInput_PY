740a1e14ed3b8026fc58fa53ac8a0194bbd86ccf
김만필 <genspark_dev@genspark.ai>
2026-04-24 02:00:19 +0000

    revert: 한글 깨짐 수정 변경사항 롤백 (73ad7ac initial commit 상태로 복원)

--- cv_stream_processor/utils/__init__.py
+++ cv_stream_processor/utils/__init__.py
@@ -1,5 +1,4 @@
 from .logger import setup_logger, app_logger
 from .fps_counter import FPSCounter
-from .korean_text import put_text_kr, get_text_size_kr
 
-__all__ = ["setup_logger", "app_logger", "FPSCounter", "put_text_kr", "get_text_size_kr"]
+__all__ = ["setup_logger", "app_logger", "FPSCounter"]
